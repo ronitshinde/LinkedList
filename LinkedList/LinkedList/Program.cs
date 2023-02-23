c26df2bda788111842c24757ff8f34cf3331b6e8
ronitshinde <rony.shinde753@gmail.com>
2023-02-23 23:03:56 +0530

    [Ronit] Add : UC1-Creating Simple Linked List

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedlist = new LinkedList();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter an option");
+                Console.WriteLine("1.Create Simple Linked List");
+                Console.WriteLine();
+                int option = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                switch (option)
+                {
+                    case 1:
+                        linkedlist.Add(56);
+                        linkedlist.Add(38);
+                        linkedlist.Add(70);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter the correct option");
+                        break;
+                }
+            }
+        }
+    }
+}
